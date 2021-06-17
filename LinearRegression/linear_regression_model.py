ae6c84e9d316adbaae6f47c5ccff3150bda9112b
jing.gao01_U+ <jing.gao01@united-imaging.com>
2021-06-17 20:40:46 +0800

    feat:增加线性回归任务

---
+++ LinearRegression/linear_regression_model.py
@@ -0,0 +1,11 @@
+'''
+Author: your name
+Date: 2021-06-17 20:31:12
+LastEditTime: 2021-06-17 20:33:33
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /Zero2OneBuildMachineLearningModel/LinearRegression/linear_regression_model.py
+'''
+class LinearRegressionModel(object):
+    def __init__(self):
+        self.name = "线性回归模型"
